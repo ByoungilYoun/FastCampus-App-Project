9a4fd9c97a38e261ab6e056414e0c7cc438244e4
YounByoungil <jimmy7525@naver.com>
2021-09-13 16:36:09 +0900

    Setup Project

---
+++ Calculator/Calculator/MainCalculatorViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainCalculatorViewController.swift
+//  Calculator
+//
+//  Created by 윤병일 on 2021/09/13.
+//
+
+import UIKit
+
+class MainCalculatorViewController : UIViewController {
+  
+  //MARK: - Properties
+  
+  //MARK: - Lifecycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    configureUI()
+  }
+  
+  //MARK: - Functions
+  private func configureUI() {
+    view.backgroundColor = .red
+  }
+}
