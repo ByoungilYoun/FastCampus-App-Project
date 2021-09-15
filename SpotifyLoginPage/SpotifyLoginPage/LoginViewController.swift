8de4b97fcbf0aebf3427aa65120ac53e9fc10f98
YounByoungil <jimmy7525@naver.com>
2021-09-15 18:04:07 +0900

    Setup Project

---
+++ SpotifyLoginPage/SpotifyLoginPage/LoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginViewController.swift
+//  SpotifyLoginPage
+//
+//  Created by 윤병일 on 2021/09/15.
+//
+
+import UIKit
+
+class LoginViewController : UIViewController {
+  
+  //MARK: - Properties
+  
+  //MARK: - Lifecycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    configureUI()
+  }
+  
+  //MARK: - Functions
+  private func configureUI() {
+    view.backgroundColor = .blue
+  }
+}
