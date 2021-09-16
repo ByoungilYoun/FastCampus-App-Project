8cfcad9836790bc4eaf80edf413ed9443a9dca37
YounByoungil <jimmy7525@naver.com>
2021-09-16 23:15:40 +0900

    이메일/비밀번호 입력 뷰 컨트롤러 기능 추가

--- SpotifyLoginPage/SpotifyLoginPage/EmailLoginController.swift
+++ SpotifyLoginPage/SpotifyLoginPage/EmailLoginController.swift
@@ -22,8 +22,10 @@ class EmailLoginController : UIViewController {
 
   let emailTextField : UITextField  = {
     let tf = UITextField()
-    tf.layer.borderWidth = 1
-    tf.layer.borderColor = UIColor.white.cgColor
+    tf.backgroundColor = .white
+    tf.textColor = .black
+    tf.layer.cornerRadius = 10
+    tf.keyboardType = .emailAddress
     return tf
   }()
   
@@ -39,16 +41,31 @@ class EmailLoginController : UIViewController {
   
   let passwordTextField : UITextField  = {
     let tf = UITextField()
-    tf.layer.borderWidth = 1
-    tf.layer.borderColor = UIColor.white.cgColor
+    tf.backgroundColor = .white
+    tf.textColor = .black
     tf.isSecureTextEntry = true
+    tf.layer.cornerRadius = 10
     return tf
+  }()
+  
+  let errorMessageLabel : UILabel = {
+    let label = UILabel()
+    label.numberOfLines = 0
+    label.textColor = .red
+    label.text = "에러입니다"
+    label.textAlignment = .center
+    label.font = UIFont.systemFont(ofSize: 18)
+    return label
   }()
   
   let nextButton : UIButton = {
     let bt = UIButton()
     bt.setTitle("다음", for: .normal)
-    bt.setTitleColor(.white, for: .normal)
+    bt.setTitleColor(.black, for: .normal)
+    bt.backgroundColor = .white
+    bt.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+    bt.layer.cornerRadius = 10
+    bt.isEnabled = false
     bt.addTarget(self, action: #selector(nextBtnTapped), for: .touchUpInside)
     return bt
   }()
@@ -58,6 +75,11 @@ class EmailLoginController : UIViewController {
     super.viewDidLoad()
     configureNavi()
     configureUI()
+    setTextFieldDelegate()
+  }
+  
+  override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    view.endEditing(true)
   }
   
   override func viewWillAppear(_ animated: Bool) {
@@ -108,15 +130,27 @@ class EmailLoginController : UIViewController {
       $0.trailing.equalToSuperview().offset(-30)
     }
     
+    view.addSubview(errorMessageLabel)
+    errorMessageLabel.snp.makeConstraints {
+      $0.top.equalTo(totalStackView.snp.bottom).offset(10)
+      $0.leading.trailing.equalTo(totalStackView)
+      $0.height.equalTo(20)
+    }
+    
     view.addSubview(nextButton)
     nextButton.snp.makeConstraints {
-      $0.top.equalTo(totalStackView.snp.bottom).offset(40)
+      $0.top.equalTo(errorMessageLabel.snp.bottom).offset(50)
       $0.centerX.equalToSuperview()
       $0.width.equalTo(80)
       $0.height.equalTo(50)
     }
   }
   
+  private func setTextFieldDelegate() {
+    emailTextField.becomeFirstResponder()
+    emailTextField.delegate = self
+    passwordTextField.delegate = self
+  }
   
   //MARK: - @objc func
   @objc func nextBtnTapped() {
@@ -124,3 +158,18 @@ class EmailLoginController : UIViewController {
   }
 }
 
+  //MARK: - extension UITextFieldDelegate
+extension EmailLoginController : UITextFieldDelegate {
+  // 리턴 버튼을 눌렀을때 키보드 내리기
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    view.endEditing(true)
+    return false
+  }
+  
+  func textFieldDidEndEditing(_ textField: UITextField) {
+    let isEmailEmpty = emailTextField.text == ""
+    let isPasswordEmpty = passwordTextField.text == ""
+    
+    nextButton.isEnabled = !isEmailEmpty && !isPasswordEmpty
+  }
+}
