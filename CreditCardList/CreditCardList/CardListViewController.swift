162aefb456ad122b4dbb83b969e6fa80254fa568
YounByoungil <jimmy7525@naver.com>
2021-09-19 22:36:12 +0900

    CardListViewController 세팅 완료

--- CreditCardList/CreditCardList/CardListViewController.swift
+++ CreditCardList/CreditCardList/CardListViewController.swift
@@ -6,10 +6,13 @@
 //
 
 import UIKit
+import SnapKit
 
 class CardListViewController : UIViewController {
   
   //MARK: - Properties
+  
+  let cardListTableView = UITableView()
   
   //MARK: - Lifecycle
   override func viewDidLoad() {
@@ -19,9 +22,39 @@ class CardListViewController : UIViewController {
   
   //MARK: - Functions
   private func configureUI() {
+    title = "카드 혜택 추천"
     view.backgroundColor = .white
+    
+    cardListTableView.delegate = self
+    cardListTableView.dataSource = self
+    cardListTableView.tableFooterView = UIView()
+    cardListTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    cardListTableView.backgroundColor = .yellow
+    view.addSubview(cardListTableView)
+   
+    cardListTableView.snp.makeConstraints {
+      $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+      $0.leading.trailing.bottom.equalToSuperview()
+    }
   }
   
   //MARK: - @objc func
+  
+}
+
+  //MARK: - UITableViewDataSource
+extension CardListViewController : UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 5
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+    return cell 
+  }
+}
+
+  //MARK: - UITableViewDelegate
+extension CardListViewController : UITableViewDelegate {
   
 }
