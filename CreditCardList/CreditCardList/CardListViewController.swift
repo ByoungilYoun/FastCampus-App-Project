d7b3ac2ef2fe64b78855937aaca0d623b02426fc
YounByoungil <jimmy7525@naver.com>
2021-09-19 22:14:05 +0900

    Reset Basic Setup

--- CreditCardList/CreditCardList/CardListViewController.swift
+++ CreditCardList/CreditCardList/CardListViewController.swift
@@ -5,4 +5,23 @@
 //  Created by 윤병일 on 2021/09/19.
 //
 
-import Foundation
+import UIKit
+
+class CardListViewController : UIViewController {
+  
+  //MARK: - Properties
+  
+  //MARK: - Lifecycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    configureUI()
+  }
+  
+  //MARK: - Functions
+  private func configureUI() {
+    view.backgroundColor = .white
+  }
+  
+  //MARK: - @objc func
+  
+}
