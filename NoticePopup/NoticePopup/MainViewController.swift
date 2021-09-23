ae2ef63b78b1139a77a349c4d7b625c1613745ab
YounByoungil <jimmy7525@naver.com>
2021-09-24 00:48:53 +0900

    Setup Project

---
+++ NoticePopup/NoticePopup/MainViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MainViewController.swift
+//  NoticePopup
+//
+//  Created by 윤병일 on 2021/09/24.
+//
+
+import UIKit
+
+class MainViewController : UIViewController {
+  
+  //MARK: - Properties
+  
+  //MARK: - Lifecycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    configureUI()
+  }
+  
+  //MARK: - Functions
+  private func configureUI() {
+    view.backgroundColor = .blue
+  }
+  
+  //MARK: - @objc func
+  
+}
