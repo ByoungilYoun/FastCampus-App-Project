acba186fd035fe344f4b163d6f669608ece21a67
YounByoungil <jimmy7525@naver.com>
2021-10-12 00:45:45 +0900

    Setup Project

---
+++ PomodoroTimerApp/PomodoroTimerApp/MainViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MainViewController.swift
+//  PomodoroTimerApp
+//
+//  Created by 윤병일 on 2021/10/12.
+//
+
+import UIKit
+
+class MainViewController : UIViewController {
+  
+  //MARK: - Properties
+  
+  //MARK: - Lifecycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    configureUI()
+  }
+  
+  //MARK: - Functions
+  private func configureUI() {
+    view.backgroundColor = .orange
+  }
+  
+  //MARK: - @objc func
+  
+}
