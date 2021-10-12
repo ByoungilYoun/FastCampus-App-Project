30b6f44c14b74cf79f0873a9a38be65ebf5d8b84
YounByoungil <jimmy7525@naver.com>
2021-10-13 01:39:32 +0900

    startTimer 와 stopTimer 함수 생성

--- PomodoroTimerApp/PomodoroTimerApp/MainViewController.swift
+++ PomodoroTimerApp/PomodoroTimerApp/MainViewController.swift
@@ -68,6 +68,8 @@ class MainViewController : UIViewController {
   
   var duration = 60 // 타이머에 설정된 시간을 초로 저장하는 프로퍼티, 60 으로 설정하는 이유는 앱이 켜질때 기본적으로 datePicker 가 1분으로 설정되어있기 때문
   var timerStatus : TimerStatus = .end // 타이머의 상태를 가지고있는 프로퍼티, end 로 초기화
+  var timer : DispatchSourceTimer?
+  var currentSeconds = 0 // 현재 카운트다운 되고 있는 시간을 초로 저장하는 프로퍼티
   
   //MARK: - Lifecycle
   override func viewDidLoad() {
@@ -130,16 +132,41 @@ class MainViewController : UIViewController {
     self.startButton.setTitle("일시정지", for: .selected)
   }
   
+  func startTimer() {
+    if self.timer == nil {
+      self.timer = DispatchSource.makeTimerSource(flags: [], queue: .main) //  우리는 ui 작업을 해줘야하기 때문에 queue 에서 main 스레드로 해야한다.
+      self.timer?.schedule(deadline: .now(), repeating: 1) // 어떤 주기로? deadline : .now -> 바로 , repeating 반복주기
+      self.timer?.setEventHandler(handler: { [weak self] in // 타이머가 동작할때마다 이벤트 핸들러가 동작한다.
+        // 즉 1초에 한번씩 여기가 구현이 된다.
+        self?.currentSeconds -= 1
+        if self?.currentSeconds ?? 0 <= 0 {
+          // 타이머가 종료
+          self?.stopTimer()
+        }
+      })
+      
+      self.timer?.resume()
+    }
+  }
+  
+  func stopTimer() {
+    if self.timerStatus == .pause { // 일시정지 상태에서 timer 에 nil 을 하려면 그 전에 resume 을 해야한다.
+      self.timer?.resume()
+    }
+    self.timerStatus = .end
+    self.cancelButton.isEnabled = false
+    self.setTimerInfoViewVisible(isHidden: true)
+    self.datePicker.isHidden = false
+    self.startButton.isSelected = false
+    self.timer?.cancel() //타이머 종료
+    self.timer = nil // 타이머를 메모리에서 해제
+  }
+  
   //MARK: - @objc func
   @objc func tapCancelButton(_ sender : UIButton) {
     switch self.timerStatus {
     case .start, .pause :
-      self.timerStatus = .end
-      self.cancelButton.isEnabled = false
-      self.setTimerInfoViewVisible(isHidden: true)
-      self.datePicker.isHidden = false
-      self.startButton.isSelected = false
-      
+      self.stopTimer()
     default :
       break
     }
@@ -149,17 +176,23 @@ class MainViewController : UIViewController {
     self.duration = Int(self.datePicker.countDownDuration) // 초로 계산되서 Int 형으로 변환
     switch self.timerStatus {
     case .end : // 시작 버튼 누를때
+      self.currentSeconds = self.duration
       self.timerStatus = .start
       self.setTimerInfoViewVisible(isHidden: false)
       self.datePicker.isHidden = true
       self.startButton.isSelected = true // 시작 버튼 타이틀이 일시정지로 변환한다.
       self.cancelButton.isEnabled = true
+      self.startTimer()
+      
     case .start :
       self.timerStatus = .pause
       self.startButton.isSelected = false // 일시정지일때 다시 시작 버튼 타이틀로 돌아오게끔
+      self.timer?.suspend() // 타이머 일시정지
+      
     case .pause :
       self.timerStatus = .start
       self.startButton.isSelected = true
+      self.timer?.resume() // 타이머 다시 시작
     }
   }
 }
