d50aac1903d10a77c07cd8bb1fe7c92d6fb96bc9
YounByoungil <jimmy7525@naver.com>
2021-10-30 17:37:30 +0900

    Setup Project

---
+++ Weather/Weather/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by 윤병일 on 2021/10/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  //MARK: - Properties
+  
+  //MARK: - Lifecycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    configureUI()
+  }
+  
+  //MARK: - Functions
+  private func configureUI() {
+    view.backgroundColor = .systemPink
+  }
+  
+  //MARK: - @objc func
+  
+
+
+}
+
