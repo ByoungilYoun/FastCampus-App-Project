d3a4927df34fab0b666079d1cc3a75bb43c63226
YounByoungil <jimmy7525@naver.com>
2021-11-14 15:59:34 +0900

    Setup Project & SPM

---
+++ Brewery/Brewery/BeerListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  BeerListViewController.swift
+//  Brewery
+//
+//  Created by 윤병일 on 2021/11/14.
+//
+
+import UIKit
+
+class BeerListViewController : UITableViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .red
+  }
+}
