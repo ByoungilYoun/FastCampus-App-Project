b5248e71a31b8b3c97be4579ed51a11cc89d3931
YounByoungil <jimmy7525@naver.com>
2021-12-13 04:16:13 +0900

    BlogListCell 구현 완료

---
+++ SearchDaumBlog/SearchDaumBlog/Entities/BlogListCellData.swift
@@ -0,0 +1,17 @@
+//
+//  BlogListCellData.swift
+//  SearchDaumBlog
+//
+//  Created by 윤병일 on 2021/12/13.
+//
+
+import Foundation
+
+struct BlogListCellData {
+  
+  let thumbnailURL : URL?
+  let name : String?
+  let title : String?
+  let dateTime : Date?
+  
+}
---
+++ SearchDaumBlog/SearchDaumBlog/Presentation/BlogList.swift
@@ -0,0 +1,8 @@
+//
+//  BlogList.swift
+//  SearchDaumBlog
+//
+//  Created by 윤병일 on 2021/12/13.
+//
+
+import Foundation
