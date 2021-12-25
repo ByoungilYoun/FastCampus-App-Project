e4fd97c190efc47d3c015bc8fbc8100703cd496f
YounByoungil <jimmy7525@naver.com>
2021-12-25 17:32:35 +0900

    Entity 구현 완료

---
+++ FindConvenienceStoreApp/FindConvenienceStoreApp/DetailListCellData.swift
@@ -0,0 +1,15 @@
+//
+//  DetailListCellData.swift
+//  FindConvenienceStoreApp
+//
+//  Created by 윤병일 on 2021/12/25.
+//
+
+import Foundation
+
+struct DetailListCellData { // 테이블뷰에 나타낼 데이터 struct 
+  let placeName : String
+  let address : String
+  let distance : String
+  let point : MTMapPoint
+}
---
+++ FindConvenienceStoreApp/FindConvenienceStoreApp/Entity/LocationData.swift
@@ -0,0 +1,12 @@
+//
+//  LocationData.swift
+//  FindConvenienceStoreApp
+//
+//  Created by 윤병일 on 2021/12/25.
+//
+
+import Foundation
+
+struct LocationData : Decodable {
+  let documents : [KLDocument]
+}
