2b211088aa9a15fd482a31b114e1709c57d274af
YounByoungil <jimmy7525@naver.com>
2022-01-24 01:57:30 +0900

    Setup Project

---
+++ MyAsset/MyAsset/MyAssetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAssetApp.swift
+//  MyAsset
+//
+//  Created by 윤병일 on 2022/01/24.
+//
+
+import SwiftUI
+
+@main
+struct MyAssetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
