c98b49aa6d13fde2f759411bd812fe33f3586cf6
YounByoungil <jimmy7525@naver.com>
2022-01-23 17:13:36 +0900

    Setup Project

---
+++ RemakeGithubRepository/RemakeGithubRepository/RepositoryListViewController.swift
@@ -0,0 +1,27 @@
+//
+//  RepositoryListViewController.swift
+//  RemakeGithubRepository
+//
+//  Created by 윤병일 on 2022/01/23.
+//
+
+import UIKit
+
+class RepositoryListViewController : UITableViewController {
+  
+  //MARK: - Properties
+  
+  //MARK: - Lifecycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    configureUI()
+  }
+  
+  //MARK: - Functions
+  private func configureUI() {
+    view.backgroundColor = .red
+  }
+  
+  //MARK: - @objc func
+  
+}
