b28e4315237526c0b9bb33b10769f862b7b84b76
YounByoungil <jimmy7525@naver.com>
2022-01-23 17:56:21 +0900

    기본 UI 작업 완료

--- RemakeGithubRepository/RemakeGithubRepository/RepositoryListViewController.swift
+++ RemakeGithubRepository/RemakeGithubRepository/RepositoryListViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class RepositoryListViewController : UITableViewController {
   
   //MARK: - Properties
+  private let organization = "Apple"
   
   //MARK: - Lifecycle
   override func viewDidLoad() {
@@ -19,9 +20,37 @@ class RepositoryListViewController : UITableViewController {
   
   //MARK: - Functions
   private func configureUI() {
-    view.backgroundColor = .red
+    title = organization + "Repositories"
+    self.refreshControl = UIRefreshControl()
+    let refreshControl = self.refreshControl
+    refreshControl?.backgroundColor = .white
+    refreshControl?.tintColor = .darkGray
+    refreshControl?.attributedTitle = NSAttributedString(string: "당겨서 새로고침")
+    refreshControl?.addTarget(self, action: #selector(refresh), for: .valueChanged)
+    
+    tableView.register(RepositoryListCell.self, forCellReuseIdentifier: RepositoryListCell.identifier)
+    tableView.rowHeight = 140
+    
   }
   
   //MARK: - @objc func
-  
+  @objc func refresh() {
+    
+  }
 }
+
+  //MARK: - UITableview Datasource, Delegate
+extension RepositoryListViewController {
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 0
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: RepositoryListCell.identifier, for: indexPath) as? RepositoryListCell else {
+      return UITableViewCell()
+    }
+    
+    return cell
+  }
+}
+ 
