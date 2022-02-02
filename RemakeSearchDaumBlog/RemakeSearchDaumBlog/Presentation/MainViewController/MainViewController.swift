cd3292b342a92318bf0734531de473bd630ba749
YounByoungil <jimmy7525@naver.com>
2022-02-03 05:27:06 +0900

    데이터 처리

--- RemakeSearchDaumBlog/RemakeSearchDaumBlog/Presentation/MainViewController/MainViewController.swift
+++ RemakeSearchDaumBlog/RemakeSearchDaumBlog/Presentation/MainViewController/MainViewController.swift
@@ -35,12 +35,85 @@ class MainViewController : UIViewController {
   
   //MARK: - Functions
   private func bind() {
+    let blogResult = searchBar.shouldLoadResult
+      .flatMapLatest { query in
+        SearchBlogNetwork().searchBlog(query: query)
+      }
+      .share() // 스트림을 쉐어 한다.
+    
+    let blogValue = blogResult
+      .compactMap { data -> DKBlog? in
+        guard case .success(let value) = data else {
+          return nil
+        }
+        
+        return value
+      }
+    
+    let blogError = blogResult
+      .compactMap { data -> String? in
+        guard case .failure(let error) = data else {
+          return nil
+        }
+        
+        return error.localizedDescription
+      }
+    
+    // 네트워크를 통해 가져온 값을 cellData 로 변환
+    let cellData = blogValue
+      .map { blog -> [BlogListCellData] in
+        return blog.documents
+          .map { doc in
+            let thumbnailURL = URL(string: doc.thumbnail ?? "")
+            return BlogListCellData(thumbnailUrl: thumbnailURL, name: doc.name, title: doc.title, datetime: doc.datetime)
+          }
+      }
+    
+    // FilterView 를 선택했을때 나오는 alertSheet를 선택했을 때 type
+    let sortedType = alertActionTapped
+      .filter {
+        switch $0 {
+        case .title, .datetime :
+          return true
+        default :
+          return false
+        }
+      }
+      .startWith(.title) // 아무것도 건들지 않으면 테이블뷰의 시작값은 타이틀 별로 할거다.
+    
+    // MainViewController -> ListView
+    Observable
+      .combineLatest(
+        sortedType,
+        cellData
+      ) { type, data -> [BlogListCellData] in
+        switch type {
+        case .title :
+          return data.sorted { $0.title ?? "" < $1.title ?? "" }
+        case .datetime :
+          return data.sorted { $0.datetime ?? Date() > $1.datetime ?? Date() }
+        default :
+          return data
+        }
+      }
+      .bind(to: listView.cellData)
+      .disposed(by: disposeBag)
+    
     let alertSheetForSorting = listView.headerView.sortButtonTapped
       .map { _ -> Alert in
         return (title : nil, message : nil, actions : [.title, .datetime, .cancel], style : .actionSheet)
       }
     
-    alertSheetForSorting
+    let alertForErrorMessage = blogError
+      .map { message -> Alert in
+        return (title : "앗!", message : "예상치 못한 오류가 발생했습니다. \(message)", actions : [.confirm], style : .alert)
+      }
+    
+    Observable
+      .merge(
+        alertSheetForSorting,
+        alertForErrorMessage
+      )
       .asSignal(onErrorSignalWith: .empty())
       .flatMapLatest { alert -> Signal<AlertAction> in
         let alertController = UIAlertController(title: alert.title, message: alert.message, preferredStyle: alert.style)
