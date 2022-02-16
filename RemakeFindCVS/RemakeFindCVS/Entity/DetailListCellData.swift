5ec7e604fc56dcdc9b02f473b4df3da9924276ef
YounByoungil <jimmy7525@naver.com>
2022-02-16 19:03:53 +0900

    Entity 생성

---
+++ RemakeFindCVS/RemakeFindCVS/Entity/DetailListCellData.swift
@@ -0,0 +1,15 @@
+//
+//  DetailListCellData.swift
+//  RemakeFindCVS
+//
+//  Created by 윤병일 on 2022/02/16.
+//
+
+import Foundation
+
+struct DetailListCellData {
+  let placeName : String
+  let address : String
+  let distance : String
+  let point : MTMapPoint
+}
---
+++ RemakeFindCVS/RemakeFindCVS/Entity/LocationData.swift
@@ -0,0 +1,12 @@
+//
+//  LocationData.swift
+//  RemakeFindCVS
+//
+//  Created by 윤병일 on 2022/02/16.
+//
+
+import Foundation
+
+struct LocationData : Decodable {
+  let documents : [KLDocument]
+}
