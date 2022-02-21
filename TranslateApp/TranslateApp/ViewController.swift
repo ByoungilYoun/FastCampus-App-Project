72c7e4acd6b5dc6eb97b5718b8a856310390db0d
YounByoungil <jimmy7525@naver.com>
2022-02-21 10:46:26 +0900

    Setup Project

---
+++ TranslateApp/TranslateApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TranslateApp
+//
+//  Created by 윤병일 on 2022/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.backgroundColor = .red
+  }
+
+
+}
+
