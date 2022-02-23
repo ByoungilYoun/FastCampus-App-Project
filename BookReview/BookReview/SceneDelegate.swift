f17ad0fe8eb2b626c31822a8f04a1d9b9b153f13
YounByoungil <jimmy7525@naver.com>
2022-02-24 01:52:20 +0900

    Setup Project

---
+++ BookReview/BookReview/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  BookReview
+//
+//  Created by 윤병일 on 2022/02/23.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+  var window: UIWindow?
+
+
+  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    guard let windowScene = (scene as? UIWindowScene) else { return }
+    
+    self.window = UIWindow(windowScene: windowScene)
+    self.window?.backgroundColor = .systemBackground
+    self.window?.rootViewController = ViewController()
+    self.window?.makeKeyAndVisible()
+  }
+}
+
---
+++ BookReview/BookReview/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  BookReview
+//
+//  Created by 윤병일 on 2022/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  //MARK: - Properties
+  
+  //MARK: - Lifecycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    configureUI()
+  }
+  
+  //MARK: - Functions
+  private func configureUI() {
+    view.backgroundColor = .yellow
+  }
+  
+  //MARK: - @objc func
+  
+}
+
