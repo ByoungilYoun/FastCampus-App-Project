b5c456903af7725055b4f293c0a96975c6ddddaa
YounByoungil <jimmy7525@naver.com>
2022-06-09 14:43:20 +0900

    [feat] BlogListData & BlogListCell 구현

---
+++ RemakeSearchDaumBlog2/RemakeSearchDaumBlog2/Entity/BlogListCellData.swift
@@ -0,0 +1,15 @@
+//
+//  BlogListCellData.swift
+//  RemakeSearchDaumBlog2
+//
+//  Created by 윤병일 on 2022/06/09.
+//
+
+import Foundation
+
+struct BlogListCellData {
+  let thumbnailURL : URL?
+  let name : String?
+  let title : String?
+  let datetime : String?
+}
---
+++ RemakeSearchDaumBlog2/RemakeSearchDaumBlog2/Presentation/BlogList.swift
@@ -0,0 +1,10 @@
+//
+//  BlogList.swift
+//  RemakeSearchDaumBlog2
+//
+//  Created by 윤병일 on 2022/06/09.
+//
+
+import Foundation
+
+
