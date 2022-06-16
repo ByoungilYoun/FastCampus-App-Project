ac0cc79189a8ba5152ba93add606bd5488f95abb
YounByoungil <jimmy7525@naver.com>
2022-06-17 01:19:12 +0900

    [UI] MainViewController 세팅

--- RemakeDanngnMarketStyleApp2/RemakeDanngnMarketStyleApp2/MainViewController.swift
+++ RemakeDanngnMarketStyleApp2/RemakeDanngnMarketStyleApp2/MainViewController.swift
@@ -6,22 +6,66 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import SnapKit
+
+typealias Alert = (title : String, message : String?)
 
 class MainViewController : UIViewController {
   
   //MARK: - Properties
+  let disposeBag = DisposeBag()
+  
+  let tableView = UITableView()
+  let submitButton = UIBarButtonItem()
   
   //MARK: - Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
-    configureUI()
+    attribute()
+    layout()
   }
   
   //MARK: - Functions
-  private func configureUI() {
+  func bind(_ viewModel : MainViewModel) {
     
   }
   
-  //MARK: - @objc func
+  private func attribute() {
+    title = "중고거래 글쓰기"
+    view.backgroundColor = .white
+    
+    submitButton.title = "제출"
+    submitButton.style = .done
+    
+    navigationItem.setRightBarButton(submitButton, animated: true)
+    
+    tableView.backgroundColor = .white
+    tableView.separatorStyle = .singleLine
+    tableView.tableFooterView = UIView()
+  }
   
+  private func layout() {
+    view.addSubview(tableView)
+    
+    tableView.snp.makeConstraints {
+      $0.edges.equalToSuperview()
+    }
+  }
+}
+
+
+  //MARK: - Reactive Extension
+
+extension Reactive where Base : MainViewController {
+  
+  var setAlert : Binder<Alert> {
+    return Binder(base) { base, data in
+      let alertController = UIAlertController(title: data.title, message: data.message, preferredStyle: .alert)
+      let action = UIAlertAction(title: "확인", style: .cancel, handler: nil)
+      alertController.addAction(action)
+      base.present(alertController, animated: true, completion: nil)
+    }
+  }
 }
